7b158f96f55d1f5f5285fc145d8c99fc8f10318e
pranith2610 <deameontargaryen@gmail.com>
2026-03-22 15:00:15 +0530

    updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/antigravity-portfolio/',
+  base: '/Pranith-Portfolio/',
   plugins: [react()],
 })
